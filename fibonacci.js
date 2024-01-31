e04e5bf1af04ed4030991b532b1fe4b2fe192588
Natalie <rivero_natalie@outlook.com>
2024-01-31 17:24:06 -0500

    completed js asgnmt

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
-  return 0;
+  let sequence = [0,1];
+  let newNum = 0;
+  for (let i = 0; i <= num; i++){
+    newNum = sequence[i] + sequence[i+1];
+    sequence.push(newNum);
+  }
+ 
+  return sequence[num]
 }
-
+console.log(fibonacci(7))
 module.exports = fibonacci;
